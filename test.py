01981b2526f50cbcaca70fc05b31aa4a3018e906
bharath209235 <kumarbharath186@gmail.com>
2025-02-14 16:41:41 +0530

    Add string concatenation function and clean up print statements in test.py

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
 print("hello WORLD   11111")
 # this is a print statement
 ##thhhh
-dgdfdfgdfgfdg
+#dgdfdfgdfgfdg
+# this is a print statement
+print("hello WORLD   11111")
+# make a function that concatenates two strings
+def concat(a, b, c):
+    return a + b + c
